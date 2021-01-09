d4d62de67ebdc0cc98b6525f45c5838626ab663e
İlkcan Anıl Çakmak <icakmak@Ilkcans-MacBook-Pro.local>
2021-01-10 01:07:52 +0300

    board component is created with its styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Board from "./components/pages/board/Board";
 
 function App() {
   return (
     <div className="App">
-        <div className="Header">React Trello Clone</div>
+        <Board />
     </div>
   );
 }
