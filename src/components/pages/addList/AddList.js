077739f31e6419778da80af611d4cae9ce8795e5
İlkcan Anıl Çakmak <icakmak@Ilkcans-MacBook-Pro.local>
2021-01-10 03:09:33 +0300

    create add list is created with its styles

---
+++ src/components/pages/addList/AddList.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import ListEditor from "../listEditor/ListEditor";
+import shortid from "shortid";
+import EditButtons from "../button/EditButtons";
+
+const AddList = (props) => {
+    const { title, setTitle } = useState("");
+
+    const handleChangeTitle = e => setTitle(e.target.value);
+
+    const createList = async () => {
+        const { dispatch } = props;
+
+        props.toggleAddingList();
+
+        dispatch({
+        type: "ADD_LIST",
+        payload: { listId: shortid.generate(), listTitle: title }
+        });
+    };
+
+    const { toggleAddingList } = props;
+
+    return (
+      <div className="Add-List-Editor">
+        <ListEditor
+          title={title}
+          handleChangeTitle={handleChangeTitle}
+          onClickOutside={toggleAddingList}
+          saveList={createList}
+        />
+
+        <EditButtons
+          handleSave={createList}
+          saveLabel={"Add list"}
+          handleCancel={toggleAddingList}
+        />
+      </div>
+    );  
+}
+
+export default connect()(AddList);
--- src/components/pages/board/Board.js
+++ src/components/pages/board/Board.js
@@ -1,18 +1,31 @@
 import "./styles.css";
 
-import React from "react";
+import React, { useState} from "react";
 import { connect } from "react-redux";
 
 import List from "../list/List";
+import AddList from "../addList/AddList";
 
 const Board = (props) => {
     const { board } = props;
+    const { addingList, setAddingList } = useState(false);
+
+    const toggleAddingList = () => setAddingList(!addingList);
 
     return (
       <div className="Board">
         {board.lists.map((listId, index) => {
           return <List listId={listId} key={listId} index={index} />;
         })}
+        <div className="Add-List">
+          {addingList ? (
+            <AddList toggleAddingList={toggleAddingList} />
+          ) : (
+            <div onClick={toggleAddingList} className="Add-List-Button">
+              <ion-icon name="add" /> Add a list
+            </div>
+          )}
+        </div>
       </div>
     );
 }
